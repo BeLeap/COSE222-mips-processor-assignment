8189d2abb4e2438d32abb7ca9ce65074298484b8
BeLeap <changseo_jang@korea.ac.kr>
2020-12-22 04:12:51 +0900

    register testbench

---
+++ test/tb_register.v
@@ -0,0 +1,25 @@
+`timescale 1ns/1ns
+
+module tb_register();
+
+reg rst, clk, RegWrite;
+reg [4:0] read1, read2, write_reg;
+reg [31:0] write_data;
+wire [31:0] data1, data2;
+
+register uut(rst, clk, read1, read2, write_reg, write_data, RegWrite, data1, data2);
+
+initial begin
+    rst = 1; clk = 0; RegWrite = 0; read1 = 0; read2 = 0; write_reg = 0; write_data = 0;
+    #50; RegWrite = 1; write_reg = 1; read1 = 1; write_data = 1;
+    #50; RegWrite = 0;
+    #50; read1 = 0; read2 = 1;
+    #50; rst = 0;
+    #50; rst = 1; read1 = 1; read2 = 0;
+end
+
+always #25 begin
+    clk = ~clk;
+end
+
+endmodule
