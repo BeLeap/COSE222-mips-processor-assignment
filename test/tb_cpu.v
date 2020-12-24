f12e3ae0dcbcb654fa1acbf0bdb55c933cfcd582
BeLeap <changseo_jang@korea.ac.kr>
2020-12-24 09:57:47 +0900

    fix cpu testbench

--- test/tb_cpu.v
+++ test/tb_cpu.v
@@ -4,8 +4,8 @@ reg rst, clk;
 // wire [6:0] seg0, seg1, seg2, seg3, seg4, seg5;
 // cpu uut(rst, clk ,seg0, seg1, seg2, seg3, seg4, seg5);
 
-wire [31:0] result, chk;
-cpu uut(rst, clk, result, chk);
+wire [6:0] seg0, seg1, seg2, seg3, seg4, seg5;
+cpu uut(rst, clk, seg0, seg1, seg2, seg3, seg4, seg5);
 
 initial begin
     rst = 0; clk = 0;
